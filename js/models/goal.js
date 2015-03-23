b53dc0c05b60facf44a14402c3581eda34ea2885
pbatani <isameer@live.com>
2015-03-23 01:09:35 -0700

    Added edit UI
    * Edit UI uses https://github.com/jdorn/json-editor
    * Moved schema for Goal to JSON schema instead of class def'n

--- js/models/goal.js
+++ js/models/goal.js
@@ -1,14 +1,2 @@
-
 var Goal = Parse.Object.extend('Goal', {
-	defaults: {
-		title: 'Untitled',
-		description: '',
-		childGoals: [],
-		primaryMetric: null,
-		secondaryMetrics: [],
-		owner: null,
-		userTimeHours: 1,
-		clockTimeHours: 1,
-		priority: 1,
-	},
-});
+		});
